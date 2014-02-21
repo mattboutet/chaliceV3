29f0faa7150adbeae1981a3f2825b1237f6933ea
Matt Boutet <matt@bigroomstudios.com>
2014-02-21 21:36:35 +0000

    Fix email config and basic email template

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'noreply@chalice.com', 'name' => 'Chalice Email'),
 
 	/*
 	|--------------------------------------------------------------------------
--- app/views/emails/register-activate.blade.php
+++ app/views/emails/register-activate.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <p>Hello {{ $user->first_name }},</p>
 
-<p>Welcome to SiteNameHere! Please click on the following link to confirm your SiteNameHere account:</p>
+<p>Welcome to ChaliceApp! Please click on the following link to confirm your ChaliceApp account:</p>
 
 <p><a href="{{ $activationUrl }}">{{ $activationUrl }}</a></p>
 
 <p>Best regards,</p>
 
-<p>SiteNameHere Team</p>
+<p>ChaliceApp Team</p>
 @stop
